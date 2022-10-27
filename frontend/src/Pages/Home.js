46c9791fd31d6314feaa9cdadaa416e2d1b05b20
sneha2180 <snehacshaji202@gmail.com>
2022-10-27 21:51:32 +0530

    Styling

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,30 +1,53 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
 
 function Home() {
-    return (
-        <div className='Home'>
-            <div className='content'>
-                <div className='content1'>
-                    <div className='div1'>
-                        <img src="/assets/frame.png" className="Navbar_image" alt="logo"></img>
-                        <div className='text1'>HANDICRAFT</div>
-                    </div>
-                    <div className='div2'>
-                        <img src="/assets/frame.png" className="Navbar_image" alt="logo"></img>
-                        <div className='text2'>PAINTINGS</div>
-                    </div>
-                    <div className='div3'>
-                        <img src="/assets/frame.png" className="Navbar_image" alt="logo"></img>
-                        <div className='text3'>DIGITAL ART</div>
-                    </div>
-                </div>
-                <div className='content2'>
-                    <img src="/assets/illustration.jpg" className="Navbar_image" alt="illustration"></img>
-                </div>
+  return (
+    <div className="Home">
+      <div className="content">
+        <div className="content1">
+          <Link to="/handicraft">
+            <div className="div1">
+              <img
+                src="/assets/frame.png"
+                className="Navbar_image"
+                alt="logo"
+              ></img>
+              <div className="text1">HANDICRAFT</div>
             </div>
+          </Link>
+          <Link to="/painting">
+            <div className="div2">
+              <img
+                src="/assets/frame.png"
+                className="Navbar_image"
+                alt="logo"
+              ></img>
+              <div className="text2">PAINTINGS</div>
+            </div>
+          </Link>
+          <Link to="/digitalart">
+            <div className="div3">
+              <img
+                src="/assets/frame.png"
+                className="Navbar_image"
+                alt="logo"
+              ></img>
+              <div className="text3">DIGITAL ART</div>
+            </div>
+          </Link>
         </div>
-    )
+        <div className="content2">
+          <img
+            src="/assets/illustration.jpg"
+            className="Navbar_image"
+            alt="illustration"
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
