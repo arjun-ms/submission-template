7db3fa99ef8f60f5d1571ffd0ea866fcfd5e6b82
Arjun M S <arjunachu123.aa@gmail.com>
2022-10-27 15:18:53 +0530

    Add User DB

---
+++ backend/src/models/User.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+
+
+export const User = sequelize.define('User', {
+  // Model attributes are defined here
+  userId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  points: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  account_balance: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  
+});
