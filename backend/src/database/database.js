1e303697f19fc20e399c2d74996e0388386ed66a
sneha2180 <snehacshaji202@gmail.com>
2022-10-27 15:13:18 +0530

    Database Configuration

---
+++ backend/src/database/database.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const { Sequelize } = require('sequelize');
+
+const configDB = {
+    "sqlite": { dialect: 'sqlite', storage: './database/data.db' },
+};
+
+const db = new Sequelize(configDB["sqlite"]);
+db.authenticate()
+    .then(() => {
+        console.log('DB Connected🚀')
+        db.sync()
+            .then((result) => {
+                console.log("Tables Synced!");
+            })
+            .catch((error) => {
+                console.log("Error: ", error);
+            })
+    })
+    .catch(err => console.log('Error: ', err));
+
+module.exports = db;
