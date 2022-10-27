b5b72878561ef3456230004758e6c9f6108a3cce
Sandra-Rosa <sandraantony2002@gmail.com>
2022-10-27 12:34:56 -0700

    Handicrafts and auctions

--- frontend/src/Components/Handicraft/Handicraft.jsx
+++ frontend/src/Components/Handicraft/Handicraft.jsx
@@ -4,40 +4,40 @@ import './Handicraft.css'
 function Handicraft() {
   return (
     <div className='Handicraft'>
-       <div className='content'>
+      <div className='content'>
         <div className='div1'>
-       <img src="/assets/Handicraft_images/Group1.png" className="Navbar_image" alt="logo"></img>
-       <div className='text1'>MASKED (BLACK) NORDIC SCULPTURE</div>
-       </div>
-       <div className='div1'>
-       <img src="/assets/Handicraft_images/Group2.png" className="Navbar_image" alt="logo"></img>
-       <div className='text1'>Handmade Terracotta Sheep Sculpture (Set Of 2)</div>
-       </div>
-       <div className='div1'>
-       <img src="/assets/Handicraft_images/Group3.png" className="Navbar_image" alt="logo"></img>
-       <div className='text1'>Indomitus Modern Bull Sculpture</div>
-       </div>
-       <div className='div1'>
-       <img src="/assets/Handicraft_images/Group4.png" className="Navbar_image" alt="logo"></img>
-       <div className='text1'>Apricus Roman Horse Sculpture</div>
-       </div>
-       <div className='div1'>
-       <img src="/assets/Handicraft_images/Group5.png" className="Navbar_image" alt="logo"></img>
-       <div className='text1'>Apricus Roman Man Sculpture</div>
-       </div>
-       <div className='div1'>
-       <img src="/assets/Handicraft_images/Group6.png" className="Navbar_image" alt="logo"></img>
-       <div className='text1'>Indomitus Angel Man Sculpture</div>
-       </div>
-       <div className='div1'>
-       <img src="/assets/Handicraft_images/Group7.png" className="Navbar_image" alt="logo"></img>
-       <div className='text1'>Blissful bubble girl sculpture</div>
-       </div>
-       <div className='div1'>
-       <img src="/assets/Handicraft_images/Group8.png" className="Navbar_image" alt="logo"></img>
-       <div className='text1'>Animus Bubble Girl Sculpture</div>
-       </div>
-       </div>
+          <img src="/assets/Handicraft_images/Group1.png" className="Navbar_image" alt="logo"></img>
+          <div className='text1'>MASKED (BLACK) NORDIC SCULPTURE</div>
+        </div>
+        <div className='div1'>
+          <img src="/assets/Handicraft_images/Group2.png" className="Navbar_image" alt="logo"></img>
+          <div className='text1'>Handmade Terracotta Sheep Sculpture (Set Of 2)</div>
+        </div>
+        <div className='div1'>
+          <img src="/assets/Handicraft_images/Group3.png" className="Navbar_image" alt="logo"></img>
+          <div className='text1'>Indomitus Modern Bull Sculpture</div>
+        </div>
+        <div className='div1'>
+          <img src="/assets/Handicraft_images/Group4.png" className="Navbar_image" alt="logo"></img>
+          <div className='text1'>Apricus Roman Horse Sculpture</div>
+        </div>
+        <div className='div1'>
+          <img src="/assets/Handicraft_images/Group5.png" className="Navbar_image" alt="logo"></img>
+          <div className='text1'>Apricus Roman Man Sculpture</div>
+        </div>
+        <div className='div1'>
+          <img src="/assets/Handicraft_images/Group6.png" className="Navbar_image" alt="logo"></img>
+          <div className='text1'>Indomitus Angel Man Sculpture</div>
+        </div>
+        <div className='div1'>
+          <img src="/assets/Handicraft_images/Group7.png" className="Navbar_image" alt="logo"></img>
+          <div className='text1'>Blissful bubble girl sculpture</div>
+        </div>
+        <div className='div1'>
+          <img src="/assets/Handicraft_images/Group8.png" className="Navbar_image" alt="logo"></img>
+          <div className='text1'>Animus Bubble Girl Sculpture</div>
+        </div>
+      </div>
     </div>
   )
 }
