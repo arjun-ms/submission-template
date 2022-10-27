08368b922d2e359a676b9763cbd3bcdb3f148cf7
Sandra-Rosa <sandraantony2002@gmail.com>
2022-10-27 08:37:58 -0700

    Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,15 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar.jsx'
+import Home from './Pages/Home';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className='content'>
-        <div className='content1'>
-          <div className='div1'>
-          <img src="/assets/frame.png" className="Navbar_image" alt="logo"></img>
-          <div className='text1'>HANDICRAFT</div>
-          </div>
-          <div className='div2'>
-          <img src="/assets/frame.png" className="Navbar_image" alt="logo"></img>
-          <div className='text2'>PAINTINGS</div>
-          </div>
-          <div className='div3'>
-          <img src="/assets/frame.png" className="Navbar_image" alt="logo"></img>
-          <div className='text3'>DIGITAL ART</div>
-          </div>
-        </div>
-        <div className='content2'>
-            <img src="/assets/illustration.jpg" className="Navbar_image" alt="illustration"></img>
-          </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
