69089055a90d7004e683172ec48e6e65ee18852c
Arjun M S <arjunachu123.aa@gmail.com>
2022-10-27 15:47:30 +0530

    Add Digitalarts DB

---
+++ backend/src/models/Digitalarts.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+const db = require("../database/database")
+
+const Digitalarts = db.define('Digitalarts', {
+  // Model attributes are defined here
+  itemId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  thumbnail: {
+    type: DataTypes.BLOB('long'),
+    allowNull: false
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  bought: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+    allowNull: false
+  },
+  artist: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+module.exports = Digitalarts
